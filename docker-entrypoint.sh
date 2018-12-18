9f41825ccb8a1bfd204ce801c08672417ba1d7bb
RazzDazz <magnus.wilde@hotmail.de>
2018-12-18 20:47:32 +0100

    Handling of first start added

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+if [ ! -f "/etc/prometheus/prometheus.yml" ] 
+then
+    echo "Copying prometheus.yml.sample to prometheus.yml for first start to volume..." 
+    cp -r /etc/prometheus/prometheus.yml.sample /etc/prometheus/prometheus.yml
+    chown prometheus:prometheus /etc/prometheus/prometheus.yml 
+fi
 
 echo "Starting supervisord to run prometheus ..." 
 exec /usr/bin/supervisord --nodaemon --configuration /tmp/supervisor_prometheus.conf
