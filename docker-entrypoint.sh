0f034e9baac33054ca6f48c6d54bb44530a3310b
RazzDazz <magnus.wilde@hotmail.de>
2019-01-05 21:06:04 +0100

    Remove dependency to user prometheus

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,7 +3,7 @@ if [ ! -f "/tmp/prometheus/prometheus.yml" ]
 then
     echo "Copying prometheus.yml.sample to prometheus.yml for first start to volume..." 
     cp /tmp/prometheus.yml.sample /tmp/prometheus/prometheus.yml
-    chown prometheus:prometheus /tmp/prometheus/prometheus.yml 
+    # chown prometheus:prometheus /tmp/prometheus/prometheus.yml 
 fi
 
 echo "Starting supervisord to run prometheus ..." 
