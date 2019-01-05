7c44c8055af72a9b42b2ab8b4ea1de7340627e85
RazzDazz <magnus.wilde@hotmail.de>
2019-01-05 21:35:28 +0100

    Move config file 'prometheus.yml'
    Move config file 'prometheus.yml' to opt/prometheus

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,7 @@
 if [ ! -f "/tmp/prometheus/prometheus.yml" ] 
 then
     echo "Copying prometheus.yml.sample to prometheus.yml for first start to volume..." 
-    cp /tmp/prometheus.yml.sample /tmp/prometheus/prometheus.yml
+    cp /tmp/prometheus.yml.sample /opt/prometheus/prometheus.yml
     # chown prometheus:prometheus /tmp/prometheus/prometheus.yml 
 fi
 
